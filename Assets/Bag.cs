4d589418acad814f0391fdbadc3a0a5a01b62474
Brice schumacher <brice.schumacher@ynov.com>
2019-11-09 11:22:08 +0100

    Add character scriptable object

---
+++ Assets/Bag.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Assets/Bag")]
+public class Bag : ScriptableObject
+{
+    Sprite sprite;
+}
---
+++ Assets/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Assets/Character")]
+public class Character : ScriptableObject
+{
+
+    public string characterName;
+    [TextArea]
+    public string backStory;
+    public List<Bag> availableBags;
+    public Bag bag;
+
+
+    public void ChooseRandomBag()
+    {
+        int bagIndex = Random.Range(0, availableBags.Count);
+        bag = availableBags[bagIndex];
+    }
+}
