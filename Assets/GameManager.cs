85a6a0a72c53473cc60836225f8ee069481c6774
Brice schumacher <brice.schumacher@ynov.com>
2019-11-09 13:29:10 +0100

    Detritus moving to scan

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject currentDetritus;
+    public Transform scanPosition;
+    public float speed = 2f;
+    bool detritusInScan;
+    public bool isScanning;
+
+    public static GameManager instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var direction = scanPosition.position - currentDetritus.transform.position;
+
+        if (direction.magnitude > 0)
+        {
+            currentDetritus.transform.position = Vector3.MoveTowards(currentDetritus.transform.position, scanPosition.position, Time.deltaTime * speed);
+        }
+        else if(!isScanning && direction.magnitude <= 0)
+        {
+            Scan();
+        }
+    }
+
+    void Scan()
+    {
+        isScanning = true;
+        Debug.Log("Je scanne le detritus");
     }
 }
--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -8,6 +8,7 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(new Vector3(0, 0, rotateSpeed));
+        if(!GameManager.instance.isScanning)
+            this.transform.Rotate(new Vector3(0, 0, rotateSpeed));
     }
 }
