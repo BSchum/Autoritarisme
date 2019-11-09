e931448f486079eedfa5faaa3ed9eefd592fd5be
eaudran <nevosis@gmail.com>
2019-11-09 14:14:16 +0100

    Init ui

---
+++ Assets/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public int environnement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text envScore = GameObject.Find("Canvas/EnvironnementText").GetComponent<Text>();
+        envScore.text = "test";
+        environnement = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
